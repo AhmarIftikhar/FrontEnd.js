4c74a55b1bdd30889afaeb61d1c593c919d8f486
AhmarIftikhar <ahmariftikhar706@gmail.com>
2023-06-06 15:47:47 +0500

    Validation

--- src/LoginForms/RegisterForm.js
+++ src/LoginForms/RegisterForm.js
@@ -36,7 +36,10 @@ function RegisterForm() {
   };
 
   const validationSchema = Yup.object().shape({
-    email: Yup.string().email("Invalid email").required("Email is required"),
+    email: Yup.string()
+      .email("Invalid email")
+      .matches(/^[^@]+@gmail\.com$/, "Email must end with gmail.com")
+      .required("Email is required"),
     password: Yup.string()
       .min(8, "Password must be at least 8 characters")
       .required("Required"),
