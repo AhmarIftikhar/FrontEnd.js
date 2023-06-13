6edc0d9dbeaf65819539640c2d2837062816e433
AhmarIftikhar <ahmariftikhar706@gmail.com>
2023-06-13 18:21:44 +0500

    few changes

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,23 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
+import { useLocation } from "react-router-dom";
 
-export default class Default extends Component {
-  render() {
-    const { pathname } = window.location;
+export default function Default() {
+  const location = useLocation();
+  const { pathname } = location;
 
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-10 mx-auto text-center text-title">
-            <h1>error</h1>
-            <h1 className="display-3">404</h1>
-            <h2>page not found</h2>
-            <h3>
-              The requested URL <span className="text-danger">{pathname}</span>{" "}
-              was not found.
-            </h3>
-          </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-10 mx-auto text-center text-title">
+          <h1>error</h1>
+          <h1 className="display-3">404</h1>
+          <h2>page not found</h2>
+          <h3>
+            The requested URL <span className="text-danger">{pathname}</span>{" "}
+            was not found.
+          </h3>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
